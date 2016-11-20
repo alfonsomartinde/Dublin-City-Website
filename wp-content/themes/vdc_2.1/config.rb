010994a598b056cb8d0336dc9a92521aa948a038
mckendu <duncan.mckenna@aerlingus.com>
2016-11-20 18:46:52 +0000

    VDC-43 removed refrences to compass in SCSS and removed most prefixes, changed SCSS output to compressed

--- wp-content/themes/vdc_2.1/config.rb
+++ wp-content/themes/vdc_2.1/config.rb
@@ -7,6 +7,7 @@ css_dir = ""
 sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
+output_style = :compressed
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
