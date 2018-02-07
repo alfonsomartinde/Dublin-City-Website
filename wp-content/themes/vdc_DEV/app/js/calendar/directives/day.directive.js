c49cdd01d2adc984869d11c49f9c0990f6798a3d
Alfonso Martin <alfonso.martin@aerlingus.com>
2018-02-07 17:21:54 +0000

    Do not make clickable days without opps

--- wp-content/themes/vdc_DEV/app/js/calendar/directives/day.directive.js
+++ wp-content/themes/vdc_DEV/app/js/calendar/directives/day.directive.js
@@ -23,8 +23,20 @@ angular.module('calendar')
         $rootScope.$emit('oppsLoaded', day.opps);
       };
     },
-    link: function(){
+    link: function(scope, elm){
 
+      function doNotMakeItClickable() {
+        elm.removeAttr('href')
+          .removeAttr('ng-click')
+          .attr('aria-role', 'presentation');
+
+        scope.select = _.noop;
+      }
+
+      // If there're no opps in this day, then do not make it clickable
+      if (_.isEmpty(scope.day.opps)) {
+        doNotMakeItClickable();
+      }
     }
   };
 });
